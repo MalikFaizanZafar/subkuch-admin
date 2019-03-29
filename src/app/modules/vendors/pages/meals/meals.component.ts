1fa83e9f5b367c691b26eb7f408f469719289267
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-03-29 14:31:56 +0500

    form validation added for item form

--- src/app/modules/vendors/pages/meals/meals.component.ts
+++ src/app/modules/vendors/pages/meals/meals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { FranchiseItemsService } from "../../services/franchiseItems.service";
 
 @Component({
@@ -31,16 +32,33 @@ export class MealsComponent implements OnInit {
   ];
   meals: any = [];
   showMeals: boolean  = true;
+  itemForm: FormGroup;
   @ViewChild('itemImage') itemImage : ElementRef;
   constructor(private franchiseItemsService: FranchiseItemsService) {}
 
   async ngOnInit() {
+    this.itemForm = new FormGroup({
+      title: new FormControl(null, [Validators.required]),
+      price: new FormControl(null, [Validators.required]),
+      discount: new FormControl(null, [Validators.required]),
+      description: new FormControl(null, [Validators.required]),
+      attachment: new FormControl(null, [Validators.required])
+    });
     await this.franchiseItemsService.getItems(50).subscribe(responseData => {
       this.meals = responseData.data;
     });
   }
 
+  onItemSubmit(form: FormGroup) {
+    console.log('form has : ', this.itemForm.value)
+    if (this.itemForm.valid) {
+      let item = this.itemForm.value;
+    } else {
+      return;
+    }
+  }
   chooseFile(){
+    console.log('choose an image')
     this.itemImage.nativeElement.click()
   }
 }
