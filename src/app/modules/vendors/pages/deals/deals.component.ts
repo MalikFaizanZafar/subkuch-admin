46671da3b220e83d43ca475eaf2d4ba1b31b3f84
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-04-25 23:32:24 +0500

    functionality for add deal done

--- src/app/modules/vendors/pages/deals/deals.component.ts
+++ src/app/modules/vendors/pages/deals/deals.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ElementRef, ViewChild  } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { FranchiseDealsService } from "../../services/franchiseDeals.service";
+import { AngularFireStorage } from "@angular/fire/storage";
+import { Observable } from "rxjs";
+import { finalize } from "rxjs/operators";
+import { dealModel } from "../../models/dealModel";
 
 @Component({
   selector: "deals",
@@ -8,11 +13,78 @@ import { FranchiseDealsService } from "../../services/franchiseDeals.service";
 })
 export class DealsComponent implements OnInit {
   deals: any = [];
-  constructor(private franchiseDealsService: FranchiseDealsService) {}
+  dealForm: FormGroup;
+  newDeal: dealModel
+  showDeals: boolean = true;
+  showEditDeal: boolean = false;
+  downloadURL: Observable<string>;
+  imageFile;
+  @ViewChild("dealImage") dealImage: ElementRef;
+  constructor(private franchiseDealsService: FranchiseDealsService, private storage: AngularFireStorage) {}
 
-  async ngOnInit() {
-    await this.franchiseDealsService.getDeals(1).subscribe(responseData => {
+  ngOnInit() {
+    this.franchiseDealsService.getDeals(1).subscribe(responseData => {
       this.deals = responseData.data;
+      console.log('this.deals has : ', this.deals)
     });
+    this.dealForm = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      price: new FormControl(1),
+      discountEnd: new FormControl(null, [Validators.required]),
+      attachment: new FormControl(null, [Validators.required])
+    });
+  }
+
+  fileChangeEvent(fileInput : any) {
+    this.imageFile = fileInput.target.files[0];
+  }
+  chooseFile() {
+    console.log("choose an image");
+    this.dealImage.nativeElement.click();
+  }
+
+  onDealSubmit(form: FormGroup) {
+    let randomString =
+      Math.random()
+        .toString(36)
+        .substring(2, 15) +
+      Math.random()
+        .toString(36)
+        .substring(2, 15);
+    const filePath = "deals/" + randomString + "-" + this.imageFile.name;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, this.imageFile);
+
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.downloadURL = fileRef.getDownloadURL();
+          this.downloadURL.subscribe(url => {
+            if (this.dealForm.valid) {
+              let deal = this.dealForm.value;
+              this.newDeal = {
+                name: deal.name,
+                price: deal.price,
+                deal_image: url,
+                end_date: deal.discountEnd,
+                franchise_id: 6
+              };
+              this.franchiseDealsService
+                .addDeal(this.newDeal)
+                .subscribe(responseData => {
+                  this.newDeal = responseData.data;
+                  this.showDeals = true;
+                  this.deals.push(this.newDeal)
+                  console.log("this.newDeal : ", this.newDeal);
+                  this.dealForm.reset();
+                });
+            } else {
+              return;
+            }
+          });
+        })
+      )
+      .subscribe();
   }
 }
--- src/app/modules/vendors/pages/meals/meals.component.ts
+++ src/app/modules/vendors/pages/meals/meals.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { FranchiseItemsService } from "../../services/franchiseItems.service";
 import { itemModel } from "../../models/itemModel";
-import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFireStorage } from "@angular/fire/storage";
 import { Observable } from "rxjs";
-import { finalize } from 'rxjs/operators';
+import { finalize } from "rxjs/operators";
 
 @Component({
   selector: "meals",
@@ -41,11 +41,14 @@ export class MealsComponent implements OnInit {
   newItem: itemModel;
   editMeal: {};
   showEditMeal: boolean = false;
-  itemUrl: String = '';
+  itemUrl: String = "";
   downloadURL: Observable<string>;
   imageFile;
   @ViewChild("itemImage") itemImage: ElementRef;
-  constructor(private franchiseItemsService: FranchiseItemsService, private storage: AngularFireStorage) {}
+  constructor(
+    private franchiseItemsService: FranchiseItemsService,
+    private storage: AngularFireStorage
+  ) {}
 
   ngOnInit() {
     this.itemForm = new FormGroup({
@@ -66,102 +69,120 @@ export class MealsComponent implements OnInit {
   }
 
   onEditItemHandler(id) {
-    let filterdItems = this.meals.filter(meal => meal.id == id)
-    this.editMeal = filterdItems[0]
-    this.showEditMeal = true
-    this.showMeals = false
-    console.log("Edit Meal is : ", this.editMeal)
+    let filterdItems = this.meals.filter(meal => meal.id == id);
+    this.editMeal = filterdItems[0];
+    this.showEditMeal = true;
+    this.showMeals = false;
+    console.log("Edit Meal is : ", this.editMeal);
   }
 
   onDeleteItemHandler(id) {
-    console.log("Delete Meal is : ", id)
+    console.log("Delete Meal is : ", id);
     this.franchiseItemsService.deleteItems(id).subscribe(response => {
-      console.log("Response from Server : ", response)
-      this.meals = this.meals.filter(meal => meal.id != id)
-    })
+      console.log("Response from Server : ", response);
+      this.meals = this.meals.filter(meal => meal.id != id);
+    });
   }
   onItemSubmit(form: FormGroup) {
-    let randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    const filePath = "items/"+randomString+"-"+this.imageFile.name;
+    let randomString =
+      Math.random()
+        .toString(36)
+        .substring(2, 15) +
+      Math.random()
+        .toString(36)
+        .substring(2, 15);
+    const filePath = "items/" + randomString + "-" + this.imageFile.name;
     const fileRef = this.storage.ref(filePath);
     const task = this.storage.upload(filePath, this.imageFile);
-    
-    task.snapshotChanges().pipe(
-      finalize(() => {
-        this.downloadURL = fileRef.getDownloadURL()
-        this.downloadURL.subscribe(url => {
-          if (this.itemForm.valid) {
-            let item = this.itemForm.value;
-            this.newItem = {
-              name: item.title,
-              description: item.description,
-              price: item.price,
-              image_url : url,
-              discount: item.discount,
-              discount_end_date: item.discountEnd,
-              available: item.isAvailable,
-              product: item.isProduct,
-              quanity: item.quantity,
-              category_id: Number(item.category),
-              franchise_id: 1,
-            };
-            this.franchiseItemsService
-              .addItem(this.newItem)
-              .subscribe(responseData => {
-                this.newItem = responseData.data;
-                this.showMeals = true
-                console.log('this.newItem : ', this.newItem)
-                this.itemForm.reset()
-              });
-          } else {
-            return;
-          }
+
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.downloadURL = fileRef.getDownloadURL();
+          this.downloadURL.subscribe(url => {
+            if (this.itemForm.valid) {
+              let item = this.itemForm.value;
+              this.newItem = {
+                name: item.title,
+                description: item.description,
+                price: item.price,
+                image_url: url,
+                discount: item.discount,
+                discount_end_date: item.discountEnd,
+                available: item.isAvailable,
+                product: item.isProduct,
+                quanity: item.quantity,
+                category_id: Number(item.category),
+                franchise_id: 1
+              };
+              this.franchiseItemsService
+                .addItem(this.newItem)
+                .subscribe(responseData => {
+                  this.newItem = responseData.data;
+                  this.showMeals = true;
+                  console.log("this.newItem : ", this.newItem);
+                  this.itemForm.reset();
+                });
+            } else {
+              return;
+            }
+          });
         })
-      } )
-   ).subscribe()
+      )
+      .subscribe();
   }
-  onEItemSubmit(form: FormGroup, mealId : any) {
-    let randomString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    const filePath = "items/"+randomString+"-"+this.imageFile.name;
+  onEItemSubmit(form: FormGroup, mealId: any) {
+    let randomString =
+      Math.random()
+        .toString(36)
+        .substring(2, 15) +
+      Math.random()
+        .toString(36)
+        .substring(2, 15);
+    const filePath = "items/" + randomString + "-" + this.imageFile.name;
     const fileRef = this.storage.ref(filePath);
     const task = this.storage.upload(filePath, this.imageFile);
-    
-    task.snapshotChanges().pipe(
-      finalize(() => {
-        this.downloadURL = fileRef.getDownloadURL()
-        this.downloadURL.subscribe(url => {
-          if (this.itemForm.valid) {
-            let item = this.itemForm.value;
-            this.newItem = {
-              name: item.title,
-              description: item.description,
-              price: item.price,
-              image_url : url,
-              discount: item.discount,
-              discount_end_date: item.discountEnd,
-              available: item.isAvailable,
-              product: item.isProduct,
-              quanity: item.quantity,
-              category_id: Number(item.category),
-              franchise_id: 1,
-            };
-            this.franchiseItemsService
-              .editItem(this.newItem, mealId)
-              .subscribe(responseData => {
-                this.newItem = responseData.data;
-                this.showEditMeal = false
-                this.showMeals = true
-                console.log('this.newItem : ', this.newItem)
-                this.itemForm.reset()
-              });
-          } else {
-            return;
-          }
+
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.downloadURL = fileRef.getDownloadURL();
+          this.downloadURL.subscribe(url => {
+            if (this.itemForm.valid) {
+              let item = this.itemForm.value;
+              this.newItem = {
+                name: item.title,
+                description: item.description,
+                price: item.price,
+                image_url: url,
+                discount: item.discount,
+                discount_end_date: item.discountEnd,
+                available: item.isAvailable,
+                product: item.isProduct,
+                quanity: item.quantity,
+                category_id: Number(item.category),
+                franchise_id: 1
+              };
+              this.franchiseItemsService
+                .editItem(this.newItem, mealId)
+                .subscribe(responseData => {
+                  this.newItem = responseData.data;
+                  this.showEditMeal = false;
+                  this.showMeals = true;
+                  console.log("this.newItem : ", this.newItem);
+                  this.itemForm.reset();
+                });
+            } else {
+              return;
+            }
+          });
         })
-      } )
-   ).subscribe()
+      )
+      .subscribe();
   }
-  fileChangeEvent(fileInput : any) {
+  fileChangeEvent(fileInput: any) {
     this.imageFile = fileInput.target.files[0];
   }
   chooseFile() {
