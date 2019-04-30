fb1ca4311ef69fc9f1dc189bc50ab6ff9e349974
malikfaizanzafar <malikfaizanzafar1993@gmail.com>
2019-04-30 10:17:07 +0500

    added vieworder dialog box

---
+++ src/app/modules/vendors/components/view-order-notification-dialog/view-order-notification-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'view-order-notification-dialog',
+  templateUrl: './view-order-notification-dialog.component.html',
+  styleUrls: ['./view-order-notification-dialog.component.css']
+})
+export class ViewOrderNotificationDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
